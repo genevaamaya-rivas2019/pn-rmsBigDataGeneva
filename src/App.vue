af523804163364cc41f856c90ea30cb3ae39059c
Geneva Rivas <rivasge_sd2053@ceaccs.net>
2019-11-15 15:47:02 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-content>
+    <Dashboard />
+    <Header />
+  </v-content>
+</template>
+
+<script>
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Header";
+
+export default {
+  name: "App",
+  components: {
+    Dashboard,
+    Header
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app id="inspire">
+    <center>
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+              <v-card class="elevation-12">
+                <v-card-text>New Requests</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+              <v-card class="elevation-12">
+                <v-card-text>Approved</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+              <v-card class="elevation-12">
+                <v-card-text>Rejected</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+              <v-card class="elevation-12">
+                <v-card-text>Pending</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
+    </center>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
